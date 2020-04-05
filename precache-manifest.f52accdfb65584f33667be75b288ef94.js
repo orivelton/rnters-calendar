729ea17626bd00b6095010a31f72093b631586d9
Orivelton <orivelton@Oriveltons-MBP.Home>
2020-04-05 18:07:26 +0100

    Updates

---
+++ precache-manifest.f52accdfb65584f33667be75b288ef94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec33144e3c328e01a9af5ab21c1d2e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e458ff6841585b9e58",
+    "url": "/static/css/2.19b7558e.chunk.css"
+  },
+  {
+    "revision": "1d9ddf89c05f768582b4",
+    "url": "/static/css/main.fbdcbeb8.chunk.css"
+  },
+  {
+    "revision": "54e458ff6841585b9e58",
+    "url": "/static/js/2.6727adf5.chunk.js"
+  },
+  {
+    "revision": "603816eec26c1aff4a38bef8d08d40a6",
+    "url": "/static/js/2.6727adf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9ddf89c05f768582b4",
+    "url": "/static/js/main.61b4034d.chunk.js"
+  },
+  {
+    "revision": "4603bbf03e3574c4d7c9",
+    "url": "/static/js/runtime-main.6e4c70f3.js"
+  }
+]);
